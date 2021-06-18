8109f9e20e4052db4b0e0be594e372c79e0e5116
NAKAJIMA Yusaku <yupong7@gmail.com>
2021-06-18 17:32:13 +0900

    add Contact

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -17,9 +17,16 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_select "title", "Help | #{@base_title}"    
   end
 
-  test "shoul get about" do
+  test "should get about" do
     get static_pages_about_url
     assert_response :success
     assert_select "title", "About | #{@base_title}"    
   end
+
+  test "should get contact" do
+    get static_pages_contact_url
+    assert_response :success
+    assert_select "title", "Contact | #{@base_title}"    
+  end
+  
 end
