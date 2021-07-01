b01abc56d15d49ec45c7ecd6116d24add9bbda28
NAKAJIMA Yusaku <yupong7@gmail.com>
2021-07-01 08:34:48 +0000

    8.1

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       
     else
-      flash[:danger] = 'Invalid email/password combination'
+      flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end 
   end 
---
+++ test/integration/users_login_test.rb
@@ -0,0 +1,13 @@
+require 'test_helper'
+
+class UsersLoginTest < ActionDispatch::IntegrationTest
+  test "login with invalid information" do
+    get login_path
+    assert_template 'sessions/new'
+    post login_path, params: { session: { email: "", password: "" } }
+    assert_template 'sessions/new'
+    assert flash.any?
+    get root_path
+    assert flash.empty?
+  end  
+end
