ab6fa9e799e85989213bc899f064f5d30444a3ab
NAKAJIMA Yusaku <yupong7@gmail.com>
2021-07-06 17:19:21 +0900

    add remembering test

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -68,5 +68,16 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     delete logout_path
     delete logout_path    
   end 
-  
+
+  test "login with remembering" do
+    log_in_as(@user, remember_me: '1')
+    assert_not_empty cookies[:remember_token]
+  end
+
+  test "login without remembering" do
+    log_in_as(@user, remember_me: '1')
+    delete logout_path
+    log_in_as(@user, remember_me: '0')
+    assert_empty cookies[:remember_token]
+  end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -13,8 +13,22 @@ class ActiveSupport::TestCase
 
   # Add more helper methods to be used by all tests here...
   include ApplicationHelper
-  
+
+  # return true if test user logged in.
   def is_logged_in?
     !session[:user_id].nil?
-  end 
+  end
+
+  # log in as user
+  def log_in_as(user)
+    session[:user_id] = user.id
+  end
+end
+
+class ActionDispatch::IntegrationTest
+  def log_in_as(user, password: 'password', remember_me: '1')
+    post login_path, params: { session: { email: user.email,
+                                          password: password,
+                                          remember_me: remember_me } }
+  end
 end
