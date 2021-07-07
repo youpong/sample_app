29eee9ae9847386e33f072c22e2e67efa075b115
NAKAJIMA Yusaku <yupong7@gmail.com>
2021-07-07 18:53:08 +0900

    add update user profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,14 @@
 class User < ApplicationRecord
   attr_accessor :remember_token
   before_save { email.downcase! }
-  validates :name,  presence: true, length: { maximum: 50 }
+  validates :name,     presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-  validates :email, presence: true, length: { maximum: 255 },
-                    format: { with: VALID_EMAIL_REGEX }, 
-                    uniqueness: true
+  validates :email,    presence: true, length: { maximum: 255 },
+                       format: { with: VALID_EMAIL_REGEX }, 
+                       uniqueness: true
   has_secure_password             
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 6 },
+                       allow_nil: true
 
   # Returns the hash value of the string
   def User.digest(string)
