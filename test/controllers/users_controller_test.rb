8a09a17ec931e5427c1356ed6ac0c9a667685d3e
NAKAJIMA Yusaku <yupong7@gmail.com>
2021-07-08 10:50:37 +0900

    add authorization

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,13 +1,44 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+
   def setup
-    @base_title = "Ruby on Rails Tutorial Sample App"
+    @base_title = "Ruby on Rails Tutorial Sample App"    
+    @user       = users(:michael)
+    @other_user = users(:archer)
   end
   
   test "should get new" do
     get signup_path
     assert_response :success
     assert_select "title", "Sign up | #{@base_title}"        
+  end
+
+  test "should redirect edit when not logged in" do
+    get edit_user_path(@user)
+    assert flash.any?
+    assert_redirected_to login_url
+  end
+
+  test "should redirect update when not logged in" do
+    patch user_path(@user), params: { user: {name:  @user.name,
+                                             email: @user.email } }
+    assert flash.any?
+    assert_redirected_to login_url
+  end
+
+  test "should redirect edit when logged in as wrong user" do
+    log_in_as(@other_user)
+    get edit_user_path(@user)
+    assert flash.empty?
+    assert_redirected_to root_url
+  end
+
+  test "should redirect update when logged in as wrong user" do
+    log_in_as(@other_user)
+    patch user_path(@user), params: { user: { name:  @user.name,
+                                              email: @user.email } }
+    assert flash.empty?
+    assert_redirected_to root_url
   end
 end
