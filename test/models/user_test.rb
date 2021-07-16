f7169931ff476b2b174a76b920707d6549a36363
NAKAJIMA Yusaku <yupong7@gmail.com>
2021-07-16 12:41:33 +0900

    implement User#feed

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -94,4 +94,23 @@ class UserTest < ActiveSupport::TestCase
     michael.unfollow(archer)
     assert_not michael.following?(archer)    
   end
+
+  test "feed should have the right posts" do
+    michael = users(:michael)
+    archer  = users(:archer)
+    lana    = users(:lana)
+
+    michael.microposts.each do |post_self|
+      assert michael.feed.include?(post_self),
+             "feed should include post by self"
+    end
+    lana.microposts.each do |post_following|
+      assert michael.feed.include?(post_following),
+             "feed should include post by following"
+    end
+    archer.microposts.each do |post_unfollowed|
+      assert_not michael.feed.include?(post_unfollowed),
+             "feed should not include post by unfollowed"
+    end
+  end
 end
