c7072bf893584ce09f90b697c77f6a8de9aa25a3
NAKAJIMA Yusaku <yupong7@gmail.com>
2021-07-18 15:49:27 +0900

    upgrade to 6.0.3.7

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_14_044729) do
+ActiveRecord::Schema.define(version: 2021_07_18_054847) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
