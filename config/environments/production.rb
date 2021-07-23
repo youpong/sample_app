00bfa488af5400a32be47e47e71630575d55db9a
NAKAJIMA Yusaku <yupong7@gmail.com>
2021-07-23 22:18:55 +0900

    get hostname from ENV.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'calm-sands-65847.herokuapp.com'
+  host = ENV['HOST_NAME']
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :port           => ENV['MAILGUN_SMTP_PORT'],
